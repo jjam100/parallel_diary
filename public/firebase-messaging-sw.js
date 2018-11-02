3be360a6b7517772b624408cfeccd38c78daf750
Chang Jae Lee <ckdwomn@live.com>
2018-11-02 20:27:40 +0900

    Add FCM Base : 이창재

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,41 @@
+importScripts('https://www.gstatic.com/firebasejs/5.5.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.2/firebase-messaging.js');
+
+firebase.initializeApp({
+    'messagingSenderId': '493885583672' //이곳은 자신의 프로젝트 설정 => 클라우드 메세징 => 발신자ID를 기입
+});
+
+const messaging = firebase.messaging();
+
+messaging.onMessage(function(payload){
+    var options = {
+        body: payload.notification.body,
+        icon: payload.notification.icon,
+        data: {
+            click_action: payload.notification.click_action
+        }
+    }
+    var notification = new Notification(payload.notification.title, options);
+});
+
+self.addEventListener('push', function(event) {
+    const payload = event.data.json();
+    const title = payload.notification.title;
+    const options = {
+        body: payload.notification.body,
+        icon: payload.notification.icon,
+        data: payload.notification.click_action
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+    console.log(event.notification);
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data)
+    );
+});
+
+console.log('[FCM Builder] active service worker found, no need to register');
